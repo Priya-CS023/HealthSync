64047b68c4b07a5cc2a676e0d13ac636c5ca7613
Sheikh-Mehedi-Hasan <mehedi.hasan.nayem@g.bracu.ac.bd>
2024-04-19 17:54:24 +0600

    User and Doctor List

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,84 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Login from './pages/login';
-import Register from './pages/register';
-import Home from './pages/home';
-import ApplyDoctor from './pages/ApplyDoctor';
-import Notifications from './pages/Notifications';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Home from "./pages/home";
+import ApplyDoctor from "./pages/ApplyDoctor";
+import Notifications from "./pages/Notifications";
+import UsersList from "./pages/Admin/UsersList";
+import DoctorsList from "./pages/Admin/DoctorsList";
 import { useSelector } from "react-redux";
-import ProtectedRoute from './components/ProtectedRoute';
-import PublicRoute from './components/PublicRoute';
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
-  const { loading } = useSelector(state => state.alerts);
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <BrowserRouter>
-      {loading && (<div className="spinner-parent">
-        <div className="spinner-border" role="status">
+      {loading && (
+        <div className="spinner-parent">
+          <div className="spinner-border" role="status"></div>
         </div>
-      </div>)}
+      )}
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-        <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/apply-doctor" element={<ProtectedRoute><ApplyDoctor/></ProtectedRoute>} />
-        <Route path="/notifications" element={<ProtectedRoute><Notifications/></ProtectedRoute>} />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/apply-doctor"
+          element={
+            <ProtectedRoute>
+              <ApplyDoctor />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/notifications"
+          element={
+            <ProtectedRoute>
+              <Notifications />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/userslist"
+          element={
+            <ProtectedRoute>
+              <UsersList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/doctorslist"
+          element={
+            <ProtectedRoute>
+              <DoctorsList />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,63 +1,67 @@
-import React from 'react';
-import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import {useDispatch} from "react-redux";
-import { useNavigate } from 'react-router-dom';
-import { showLoading, hideLoading } from '../redux/alertsSlice';
-
-
+import React from "react";
+import { Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { showLoading, hideLoading } from "../redux/alertsSlice";
 
 function Login() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const onFinish = async (values) => {
-        try {
-            dispatch(showLoading());
-            const response = await axios.post('/api/user/login', values);
-            dispatch(hideLoading());
-            if (response.data.success) {
-                toast.success(response.data.message);
-                toast("Redirecting to home page...");
-                localStorage.setItem("token", response.data.token);
-                navigate("/");
-            } else {
-                toast.error(response.data.message);
-            }
-        } catch (error) {
-            dispatch(hideLoading());
-            if (error.response) {
-                toast.error(error.response.data.message);
-            } else if (error.request) {
-                toast.error("No response received from the server");
-            } else {
-                toast.error("Something went wrong");
-            }
-        }
-    };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.post("/api/user/login", values);
+      dispatch(hideLoading());
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to home page...");
+        localStorage.setItem("token", response.data.token);
+        navigate("/");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else if (error.request) {
+        toast.error("No response received from the server");
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
+  };
 
-    return (
-        <div className='authentication'>
-            <div className='authentication-form card p-3'>
-                <h1 className="card-title">Welcome Back!</h1>
+  return (
+    <div className="authentication">
+      <div className="authentication-form card p-3">
+        <h1 className="card-title">Welcome Back!</h1>
 
-                <Form layout='vertical' onFinish={onFinish}>
-                    <Form.Item label='Email' name='email'>
-                        <Input placeholder='Email' />
-                    </Form.Item>
-                    <Form.Item label='Password' name='password'>
-                        <Input placeholder='Password' type='password' />
-                    </Form.Item>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item label="Email" name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
 
-                    <Button className='primary-button my-2' htmlType='submit'>LOGIN</Button>
+          <Button
+            className="primary-button my-2 full-width-button"
+            htmlType="submit"
+          >
+            LOGIN
+          </Button>
 
-                    <Link to='/register' className='anchor mt-2'>CLICK HERE TO REGISTER</Link>
-
-                </Form>
-            </div>
-        </div>
-    );
+          <Link to="/register" className="anchor mt-2">
+            CLICK HERE TO REGISTER
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
